f21355a3f32ace1893bbccd2f1336804c951f547
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2025-07-30 19:57:45 -0400

    fix

--- mensage.js
+++ mensage.js
@@ -1,8 +1,15 @@
 window.onload = () =>{ 
 const nuevo = document.createElement('p');
 nuevo.textContent = 'THE BEST PRICES IN THE AREA.';
+nuevo.classList.add('fade-in');
 setTimeout(() => {
     document.getElementById('nuevomensaje').appendChild(nuevo);
+
+     
+    void nuevo.offsetWidth;
+    nuevo.classList.add('visible');
+
+
 }, 4000);
 
 };
