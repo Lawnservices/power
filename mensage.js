8c56301594d17506a2c2919be7c375ef6f114d4f
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2025-07-30 16:55:23 -0400

    fix

--- mensage.js
+++ mensage.js
@@ -1,3 +1,3 @@
 const nuevo = document.createElement('p');
-nuevo.textContent = 'Unbuen servicio de gramma llamanos hoy';
+nuevo.textContent = 'we have the best prices';
 document.getElementById('nuevomensaje').appendChild(nuevo);
